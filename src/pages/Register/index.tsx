1f707e06b466af1347cc44221638e4dd272f68d6
Marco Paiva <tulio.paiva10@gmail.com>
2024-02-03 19:33:36 -0300

    (feat/login): Creating login, register, logout, forgotPassword methods

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,11 +1,94 @@
 // Routes.tsx
-import React from 'react';
+import React from "react";
+import MainLayout from "../../components/layouts/MainLayout";
+import { Card, Space, Row, Col } from "antd";
+
+import { Button, Checkbox, Form, Input } from "antd";
+
+const onFinish = (values: any) => {
+  console.log("Success:", values);
+};
+
+const onFinishFailed = (errorInfo: any) => {
+  console.log("Failed:", errorInfo);
+};
+
+type FieldType = {
+  username?: string;
+  password?: string;
+  remember?: string;
+};
 
 const Register: React.FC = () => {
   return (
-    <div>
-        <h1>Register</h1>
-    </div>
+    <MainLayout>
+      <Row justify="center" style={{ minHeight: "100vh" }}>
+        <Col>
+        <Card
+            title="Sign in to WebCars"
+            style={{ width: 400 }}
+            headStyle={{ backgroundColor: '#1890ff', color: 'white' }}
+            className="shadow-lg"
+          >
+            <Form
+              name="basic"
+              labelCol={{ span: 8 }}
+              wrapperCol={{ span: 16 }}
+              style={{ maxWidth: 600 }}
+              initialValues={{ remember: true }}
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+              autoComplete="off"
+            >
+              <Form.Item
+                name="email"
+                label="E-mail"
+                rules={[
+                  {
+                    type: "email",
+                    message: "The input is not valid E-mail!",
+                  },
+                  {
+                    required: true,
+                    message: "Please input your E-mail!",
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                  { required: true, message: "Please input your password!" },
+                ]}
+              >
+                <Input.Password />
+              </Form.Item>
+
+              <Form.Item
+                name="remember"
+                valuePropName="checked"
+                wrapperCol={{ offset: 8, span: 16 }}
+              >
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+
+              <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  style={{ backgroundColor: "#1890ff", borderColor: "#1890ff" }}
+                >
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+          </Card>
+        </Col>
+      </Row>
+    </MainLayout>
   );
 };
 
